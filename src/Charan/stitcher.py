c3bf04f059407f55cdfe42e626a056ba5524e794
Charan teja Koppolu <charantejakoppolu@gmail.com>
2024-10-28 21:55:13 +0530

    commit

--- src/Charan/stitcher.py
+++ src/Charan/stitcher.py
@@ -153,57 +153,7 @@ class PanaromaStitcher():
     def __init__(self):
         self.sift_detector = cv2.SIFT_create()
         self.bf_matcher = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
-
-    def extract_keypoints_and_descriptors(self, img):
-        """Detect keypoints and compute descriptors using SIFT"""
-        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        keypoints, descriptors = self.sift_detector.detectAndCompute(gray_img, None)
-        return keypoints, descriptors
-
-    def find_homography(self, image1, image2):
-        """ Find homography matrix using RANSAC with 5 point correspondences"""
-        keypoints1, descriptors1 = self.extract_keypoints_and_descriptors(image1)
-        keypoints2, descriptors2 = self.extract_keypoints_and_descriptors(image2)
-
-        matches = self.bf_matcher.match(descriptors1, descriptors2)
-        matches = sorted(matches, key=lambda x: x.distance)
-
-        points1 = np.float32([keypoints1[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
-        points2 = np.float32([keypoints2[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
-
-        homography_matrix, _ = cv2.findHomography(points1, points2, cv2.RANSAC, 5.0)
-        return homography_matrix
-
-    def calculate_homographies(self, img_list):
-        """find homographies between consecutive images"""
-        homographies_list = [np.eye(3)] 
-        for i in range(len(img_list) - 1):
-            homography = self.find_homography(img_list[i], img_list[i + 1])
-            homographies_list.append(homography / homography[-1, -1]
-)
-        return homographies_list
-
-    def accumulate_homographies(self, homographies_list, target_index=1):
-        """Compute total homographies relative to a target image"""
-        accumulated_homographies = []
-        identity_matrix = np.eye(3) 
-
-        for idx, homography in enumerate(homographies_list):
-            if idx < target_index:
-                accumulated_homography = np.eye(3)
-                for j in range(idx, target_index):
-                    accumulated_homography = np.dot(homographies_list[j + 1], accumulated_homography)
-                accumulated_homographies.append(accumulated_homography)
-            elif idx > target_index:
-                accumulated_homography = np.eye(3)
-                for j in range(idx, target_index, -1):
-                    accumulated_homography = np.dot(np.linalg.inv(homographies_list[j]), accumulated_homography)
-                accumulated_homographies.append(accumulated_homography)
-            else:
-                accumulated_homographies.append(identity_matrix)
-
-        return accumulated_homographies
-
+        
     def make_panoroma_for_images_in(self, path):
         """Read images from the path, use precomputed homographies, and create a panorama"""
         
@@ -236,3 +186,53 @@ class PanaromaStitcher():
             panorama_img = np.maximum(panorama_img, warped_img)
 
         return panorama_img, homographies
+
+    def extract_keypoints_and_descriptors(self, img):
+        """Detect keypoints and compute descriptors using SIFT"""
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        keypoints, descriptors = self.sift_detector.detectAndCompute(gray_img, None)
+        return keypoints, descriptors
+
+    def find_homography(self, image1, image2):
+        """ Find homography matrix using RANSAC with 5 point correspondences"""
+        keypoints1, descriptors1 = self.extract_keypoints_and_descriptors(image1)
+        keypoints2, descriptors2 = self.extract_keypoints_and_descriptors(image2)
+
+        matches = self.bf_matcher.match(descriptors1, descriptors2)
+        matches = sorted(matches, key=lambda x: x.distance)
+
+        points1 = np.float32([keypoints1[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
+        points2 = np.float32([keypoints2[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
+
+        homography_matrix, _ = cv2.findHomography(points1, points2, cv2.RANSAC, 5.0)
+        return homography_matrix
+
+    def calculate_homographies(self, img_list):
+        """find homographies between two consecutive images"""
+        homographies_list = [np.eye(3)] 
+        for i in range(len(img_list) - 1):
+            homography = self.find_homography(img_list[i], img_list[i + 1])
+            homographies_list.append(homography / homography[-1, -1]
+)
+        return homographies_list
+
+    def accumulate_homographies(self, homographies_list, target_index=1):
+        """Compute total homographies relative to a target image"""
+        accumulated_homographies = []
+        identity_matrix = np.eye(3) 
+
+        for idx, homography in enumerate(homographies_list):
+            if idx < target_index:
+                accumulated_homography = np.eye(3)
+                for j in range(idx, target_index):
+                    accumulated_homography = np.dot(homographies_list[j + 1], accumulated_homography)
+                accumulated_homographies.append(accumulated_homography)
+            elif idx > target_index:
+                accumulated_homography = np.eye(3)
+                for j in range(idx, target_index, -1):
+                    accumulated_homography = np.dot(np.linalg.inv(homographies_list[j]), accumulated_homography)
+                accumulated_homographies.append(accumulated_homography)
+            else:
+                accumulated_homographies.append(identity_matrix)
+
+        return accumulated_homographies
